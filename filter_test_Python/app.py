b21ca6a0c146cf91fce70f99f1eed3bc5aaf1150
AGSugar <guanshuao@users.noreply.github.com>
2023-05-03 21:04:52 +0800

    居中显示，优化

--- filter_test_Python/app.py
+++ filter_test_Python/app.py
@@ -1,7 +1,7 @@
 import os
 import tkinter as tk
 from tkinter import filedialog
-from goldstein_filter import main as goldstein_filter_main, read_interferogram
+from goldstein_filter import main as goldstein_filter_main
 from gen_tif import main as gen_tif_main
 from lxml import etree
 from PIL import Image, ImageTk
@@ -11,53 +11,56 @@ class App(tk.Tk):
         super().__init__()
 
         self.title("Goldstein Filter & TIF Generator")
-        self.geometry("1200x600")
+        self.geometry("800x600")
 
+        self.create_widgets()
+
+    def create_widgets(self):
         self.file_name = tk.StringVar()
         self.filter_strength = tk.DoubleVar(value=0.5)
         self.window_size = tk.IntVar(value=32)
         self.step_size = tk.IntVar(value=8)
-
-        tk.Label(self, text="Input file:").grid(row=0, column=0, sticky="w")
-        tk.Entry(self, textvariable=self.file_name).grid(row=0, column=1, sticky="ew")
-        tk.Button(self, text="Browse", command=self.browse_file).grid(row=0, column=2)
-
-        tk.Label(self, text="Filter strength:").grid(row=1, column=0, sticky="w")
-        tk.Entry(self, textvariable=self.filter_strength).grid(row=1, column=1, sticky="ew")
-
-        tk.Label(self, text="Window size:").grid(row=2, column=0, sticky="w")
-        tk.Entry(self, textvariable=self.window_size).grid(row=2, column=1, sticky="ew")
-
-        tk.Label(self, text="Step size:").grid(row=3, column=0, sticky="w")
-        tk.Entry(self, textvariable=self.step_size).grid(row=3, column=1, sticky="ew")
-
-        tk.Button(self, text="Apply Filter & Generate TIF", command=self.apply_filter_and_generate_tif).grid(row=4, columnspan=3)
-
         self.output_info = tk.StringVar()
-        tk.Label(self, textvariable=self.output_info).grid(row=5, columnspan=3)
 
-        self.before_image_label = tk.Label(self)
-        self.before_image_label.grid(row=6, column=0, columnspan=3)
+        tk.Label(self, text="Input file:").pack()
+        tk.Entry(self, textvariable=self.file_name).pack()
+        tk.Button(self, text="Browse", command=self.browse_file).pack()
 
-        self.after_image_label = tk.Label(self)
-        self.after_image_label.grid(row=6, column=3, columnspan=3)
+        tk.Label(self, text="Filter strength:").pack()
+        tk.Entry(self, textvariable=self.filter_strength).pack()
 
-        self.columnconfigure(1, weight=1)
+        tk.Label(self, text="Window size:").pack()
+        tk.Entry(self, textvariable=self.window_size).pack()
 
+        tk.Label(self, text="Step size:").pack()
+        tk.Entry(self, textvariable=self.step_size).pack()
 
+        tk.Button(self, text="Apply Filter & Generate TIF", command=self.apply_filter_and_generate_tif).pack()
+
+        tk.Label(self, textvariable=self.output_info).pack()
+
+        image_frame = tk.Frame(self)
+        image_frame.pack(pady=10)
+
+        before_image_container = tk.Frame(image_frame)
+        before_image_container.pack(side=tk.LEFT, padx=10)
+
+        self.before_image_label = tk.Label(before_image_container, image=None)
+        self.before_image_label.pack()
+        tk.Label(before_image_container, text="Original Image").pack()
+
+        after_image_container = tk.Frame(image_frame)
+        after_image_container.pack(side=tk.RIGHT, padx=10)
+
+        self.after_image_label = tk.Label(after_image_container, image=None)
+        self.after_image_label.pack()
+        self.filter_info_label = tk.Label(after_image_container, text="")
+        self.filter_info_label.pack()
 
     def browse_file(self):
         file_path = filedialog.askopenfilename()
         if file_path:
             self.file_name.set(file_path)
-
-    def display_image(self, file_path, label):
-        img = Image.open(file_path)
-        img.thumbnail((500, 500))  # Resize the image to fit the GUI
-        photo = ImageTk.PhotoImage(img)
-
-        label.config(image=photo)
-        label.image = photo
 
     def apply_filter_and_generate_tif(self):
         if not self.file_name.get():
@@ -82,12 +85,23 @@ class App(tk.Tk):
         output_message = f"Filtered and generated TIF for {self.file_name.get()} with filter strength {self.filter_strength.get()}, window size {self.window_size.get()}, and step size {self.step_size.get()}"
         self.output_info.set(output_message)
 
-        # Display the images
-        input_tif_file = self.file_name.get().replace('.int', '.int.tif')
-        self.display_image(input_tif_file, self.before_image_label)
-        self.display_image(output_file + ".tif", self.after_image_label)
+        filter_info = f"Filtered Image (f={self.filter_strength.get()}, w={self.window_size.get()}, s={self.step_size.get()})"
+        self.filter_info_label.config(text=filter_info)
+
+        # 显示原始图像
+        before_image = Image.open(self.file_name.get().replace(".int", ".int.tif"))
+        before_image.thumbnail((200, 200), Image.LANCZOS)
+        before_photo = ImageTk.PhotoImage(before_image)
+        self.before_image_label.config(image=before_photo)
+        self.before_image_label.image = before_photo
+
+        # 显示滤波后的图像
+        after_image = Image.open(output_file.replace(".int", ".int.tif"))
+        after_image.thumbnail((200, 200), Image.LANCZOS)
+        after_photo = ImageTk.PhotoImage(after_image)
+        self.after_image_label.config(image=after_photo)
+        self.after_image_label.image = after_photo
 
 if __name__ == "__main__":
     app = App()
     app.mainloop()
-
