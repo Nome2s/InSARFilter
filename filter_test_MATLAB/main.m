5016b62fdc3b2e849f0b89ebd4f26925f51245d7
AGSugar <guanshuao@users.noreply.github.com>
2023-05-03 22:50:59 +0800

    GUI界面

--- filter_test_MATLAB/main.m
+++ filter_test_MATLAB/main.m
@@ -1,32 +1,85 @@
-close all;clear;clc;
-% 图像长宽
-width=2551;
-length=2108;
+function main()
+    % 图像宽度
+    width = 2551;
+    length = 2108;
+    
+    % 创建主界面
+    fig = uifigure('Name', 'Goldstein滤波', 'Position', [100 100 400 250]);
 
-% 读取数据
-data=read_int('data/original/original.int',width);
-phase=angle(data);
+    % 创建文件路径标签
+    filePathLabel = uilabel(fig, 'Position', [100 200 250 22], 'Text', '待处理文件路径:');
 
-% 显示滤波前的干涉图
-figure,imagesc(phase,[-pi,pi]);colormap('jet');colorbar;
-title('Original Image');
+    % 创建浏览按钮
+    browseButton = uibutton(fig, 'Text', '浏览', 'Position', [50 200 50 22], 'ButtonPushedFcn', @browseButtonPushed);
 
-% 滤波
-data_filter=goldstein_filter(data,0.5,32,8);
+    % 创建滤波参数输入框和标签
+    alphaEditField = uieditfield(fig, 'numeric', 'Position', [50 140 80 22], 'Value', 0.5);
+    alphaLabel = uilabel(fig, 'Position', [50 165 80 22], 'Text', '滤波参数 (alpha):');
 
-% 显示滤波后的干涉图
-phase_out=angle(data_filter);
-figure,imagesc(phase_out,[-pi,pi]);colormap('jet');colorbar;
-title('Filtered Image')
+    % 创建滑动窗口大小输入框和标签
+    windowSizeEditField = uieditfield(fig, 'numeric', 'Position', [180 140 80 22], 'Value', 32);
+    windowSizeLabel = uilabel(fig, 'Position', [180 165 80 22], 'Text', '滑动窗口大小:');
 
-% 计算滤波前后的差值并显示
-diff=phase_out-phase;
-figure,imagesc(diff);colormap('jet');colorbar;
-title('Phase Difference')
+    % 创建滑动窗口步长输入框和标签
+    stepSizeEditField = uieditfield(fig, 'numeric', 'Position', [310 140 80 22], 'Value', 8);
+    stepSizeLabel = uilabel(fig, 'Position', [310 165 80 22], 'Text', '滑动窗口步长:');
 
-% 保存滤波后的结果
-write_int('data/out.int',data_filter);
+    % 创建处理按钮
+    processButton = uibutton(fig, 'Text', '处理', 'Position', [160 50 80 22], 'ButtonPushedFcn', @processButtonPushed);
 
-% 输出图像，jet色带
-phase2raster(data,'data/original.int.tif');
-phase2raster(data_filter,'data/out.int.tif');
+    % 处理按钮的回调函数
+    function processButtonPushed(~, ~)
+        % 获取用户输入的滤波参数、滑动窗口大小和滑动窗口步长
+        alpha = alphaEditField.Value;
+        windowSize = windowSizeEditField.Value;
+        stepSize = stepSizeEditField.Value;
+
+        % 获取待处理的文件
+        file = filePathLabel.Text;
+        if ~strcmp(file, '待处理文件路径:')
+            % 构造输出文件夹路径
+            folderName = sprintf('filter_f%.1f_w%d_s%d', alpha, windowSize, stepSize);
+            outputPath = fullfile('./data', folderName);
+
+            % 如果输出文件夹不存在，则创建
+            if ~exist(outputPath, 'dir')
+                mkdir(outputPath);
+            end
+
+            % 读取干涉图复数数据
+            data = read_int(file, width);
+
+            % 进行Goldstein滤波
+            filteredData = goldstein_filter(data, alpha, windowSize, stepSize);
+
+            % 构造输出文件路径
+            [~, name, ~] = fileparts(file);
+            intOutputPath = fullfile(outputPath, [name '.int']);
+            imageOutputPath = fullfile(outputPath, [name '.jpeg']);
+
+            % 保存滤波后的int文件
+            write_int(intOutputPath, filteredData);
+
+            %
+            % 将滤波后的干涉图保存为图像
+            phase2raster(filteredData, imageOutputPath);
+
+            % 显示处理完成的消息对话框
+            uialert(fig, '处理完成！', '提示', 'Icon', 'success');
+        else
+            uialert(fig, '请选择一个待处理的文件', '提示', 'Icon', 'warning');
+        end
+    end
+
+    % 浏览按钮的回调函数
+    function browseButtonPushed(~, ~)
+        % 打开文件选择对话框
+        [file, path] = uigetfile('*.int', '选择待处理的文件');
+
+        % 如果选择了文件，则更新文件路径显示
+        if file ~= 0
+            filePath = fullfile(path, file);
+            filePathLabel.Text = filePath;
+        end
+    end
+end
