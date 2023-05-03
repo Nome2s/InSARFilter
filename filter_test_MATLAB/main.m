79781bae33fb085ac6b56e233a0407b03a2a954d
AGSugar <guanshuao@users.noreply.github.com>
2023-05-03 22:24:10 +0800

    改输出路径

--- filter_test_MATLAB/main.m
+++ filter_test_MATLAB/main.m
@@ -1,14 +1,15 @@
 close all;clear;clc;
-% 宽度和行数
+% 图像长宽
 width=2551;
 length=2108;
 
 % 读取数据
-data=read_int('data/diff_150405-150503_10rlks_56alks.int',width);
+data=read_int('data/original/original.int',width);
 phase=angle(data);
 
 % 显示滤波前的干涉图
 figure,imagesc(phase,[-pi,pi]);colormap('jet');colorbar;
+title('Original Image');
 
 % 滤波
 data_filter=goldstein_filter(data,0.5,32,8);
@@ -16,10 +17,12 @@ data_filter=goldstein_filter(data,0.5,32,8);
 % 显示滤波后的干涉图
 phase_out=angle(data_filter);
 figure,imagesc(phase_out,[-pi,pi]);colormap('jet');colorbar;
+title('Filtered Image')
 
 % 计算滤波前后的差值并显示
 diff=phase_out-phase;
 figure,imagesc(diff);colormap('jet');colorbar;
+title('Phase Difference')
 
 % 保存滤波后的结果
 write_int('data/out.int',data_filter);
