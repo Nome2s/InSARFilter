9986d076ec812c21d3b39b36e8b8790db63917e5
AGSugar <guanshuao@users.noreply.github.com>
2023-05-04 11:58:18 +0800

    精简Repo

--- test_Python/read_int.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def read_int(file_name, width):
-    data = np.fromfile(file_name, dtype=np.float32)
-    count = len(data)
-    data = data[0::2] + 1j * data[1::2]
-    lines = count // (width * 2)
-    data = data.reshape((width, lines)).T
-    return data
